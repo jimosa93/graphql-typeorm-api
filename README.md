78db55265066170b44b5fb58b3b1b268a9915336
Jorge Morales <jimosa93@gmail.com>
2020-07-17 15:42:07 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Typescript GraphQL & TypeORM
+
+> This is a GraphQL API using ApolloServer, Typescript and Mysql
+
+## Quick Start
+
+```bash
+# Install dependencies
+npm install
+
+# Serve on localhost:3000
+npm run dev
+```
---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { PingResolver } from './resolvers/ping';
+import { buildSchema } from 'type-graphql';
+import { ProductResolver } from './resolvers/ProductResolver';
+
+export async function startServer() {
+  const app = express();
+
+  const server = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [PingResolver, ProductResolver],
+      validate: false
+    }),
+    context: ({ req, res }) => ({ req, res })
+  });
+
+  server.applyMiddleware({ app, path: '/graphql' });
+
+  return app;
+}
